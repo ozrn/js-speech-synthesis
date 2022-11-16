20f2e19027337533a3783600ef5cd005f6fb1480
ozrn <ozerenkevser92@gmail.com>
2022-11-16 12:14:29 +0300

    Retrieve all voices available

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 const msg = new SpeechSynthesisUtterance();
-let voices = [];
+let voices = []; // our voices are going to be dumped into
 const voicesDropdown = document.querySelector("[name='voice']");
 const options = document.querySelectorAll("[type='range'], [name='text']");
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
 
 msg.text = document.querySelector("[name = 'text']").value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  console.log(voices);
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
