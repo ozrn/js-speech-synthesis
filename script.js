7dbf9b40efdce22544ce8e3239213979c4ba7d8a
ozrn <ozerenkevser92@gmail.com>
2022-11-16 20:13:22 +0300

    Create fn to stop speaking and start it again

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ msg.text = document.querySelector("[name = 'text']").value;
 function populateVoices() {
   voices = this.getVoices();
   voicesDropdown.innerHTML = voices
-      .map(voice => `<option value=${voice.name}>${voice.name} ${voice.lang}</option>`)
+      .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
       .join("");
 }
 
 function setVoice(){
   msg.voice = voices.find(voice => voice.name === this.value); // this is every option in the dropdown menu
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel(); // stop it from speaking
+  if(startOver){
+    speechSynthesis.speak(msg); // restart the speech
+  }
 }
 
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
